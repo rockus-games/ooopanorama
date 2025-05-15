1e5c47027a9be06d2beb967aff928bcd42cae5b2
Alex Tepanosyan <aetepanosyan@gmail.com>
2025-05-15 14:00:43 +0300

    Admin panel start

---
+++ php/login.php
@@ -0,0 +1,25 @@
+<?php
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+
+    $conn = new mysqli("92.53.96.223:3306", "cb07184_panorama", "9BWv5j430", "cb07184_panorama");
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $query = "SELECT * FROM users WHERE login = '$login'";
+    $data = $conn->query($query);
+
+    if($data->num_rows > 0) {
+        while($row = $data->fetch_assoc()) {
+            if($row["password"] == md5($password)) {
+                echo json_encode($row);
+                $conn->close();
+                exit();
+            }
+        }
+    }
+
+    die("No users found");
+?>
