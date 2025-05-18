1584dffa6cf11d22d6f443c73a9c8f9274818980
Alex Tepanosyan <aetepanosyan@gmail.com>
2025-05-19 00:22:45 +0300

    Php fix

--- php/get_data.php
+++ php/get_data.php
@@ -1,7 +1,7 @@
 <?php
     $table = $_POST["table"];
 
-    $conn = new mysqli("92.53.96.223:3306", "cb07184_panorama", "9BWv5j430", "cb07184_panorama");
+    $conn = new mysqli("localhost:3306", "cb07184_panorama", "9BWv5j430", "cb07184_panorama");
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
