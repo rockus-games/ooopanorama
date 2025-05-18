ace7e0bc21cd4920dfbdf976c6ef0d3218333539
Alex Tepanosyan <aetepanosyan@gmail.com>
2025-05-18 23:07:18 +0300

    Balconies

--- js/is_mobile.js
+++ js/is_mobile.js
@@ -3,3 +3,13 @@ function isMobile() {
         /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
     return regex.test(navigator.userAgent);
 }
+
+function toggleSideMenu() {
+    var menu = document.querySelector("#mobileNavMenu");
+
+    if (menu.className == "mobileNavMenuClosed") {
+        menu.className = "mobileNavMenuOpened";
+    } else {
+        menu.className = "mobileNavMenuClosed";
+    }
+}
